98b5178a0aa08d323205554198672a8cef41f987
Leah Hersh <lhersh411@gmail.com>
2020-03-27 17:22:39 -0400

    Initial Commit

---
+++ Sloths!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sloths!
+//
+//  Created by admin on 3/24/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
